d8de0ce7c532f83aaad58763af473254950f1a7d
FrauleinElfeElfe <dassarahwriedt@gmail.com>
2026-05-12 16:41:26 +0200

    aaaAAAAAaaaaAAAAAAhhhhHHHH

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -119,11 +119,10 @@ export default function Login() {
           <br /><br />
           <strong>2. Toxicity.</strong><br />
           To reduce toxicity lokali is: <br />
-	  <ul>
-	  	<li>strictly moderated <br /> </li>
-	  	<li>only available when you have the link (given from friends to friends/ mooties to mooties)<br /></li>
-	  	<li>only partially anonymous, yet anonymous enough that the focus stays on interactions, rather than self-promotion and competition. 🌳</li>
-	  </ul>
+	  	<li style = "list-style-type: lower-alpha;">strictly moderated <br /> </li>
+	  	<li style = "list-style-type: lower-alpha;">only available when you have the link <br />
+		(given from friends to friends/ mooties to mooties)<br /></li>
+	  	<li style = "list-style-type: lower-alpha;">only partially anonymous, yet anonymous enough that the focus stays on interactions, rather than self-promotion and competition. 🌳</li>
         </p>
       </div>
 
